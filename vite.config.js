fb21df03a41b5387e307e1308e94d7f7917bc722
Zander-Oelofse <zanderoelofse13@gmail.com>
2026-02-01 15:29:53 +0200

    configs

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      // This MUST match your tsconfig.json "paths"
+      '@': path.resolve(__dirname, './src'),
     },
   },
-})
+  build: {
+    // Ensure this matches the 'directory' in wrangler.jsonc
+    outDir: 'dist',
+  },
+});
